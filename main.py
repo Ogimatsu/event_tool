7efe74f6607a44975f0ed0bdaf468d66ebb66622
Ogimatsu <ogimattsu@gmail.com>
2025-02-07 00:35:53 +0900

    画面の最初の準備

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.text import LabelBase
 from kivy.lang import Builder
+from kivy.properties import StringProperty, ObjectProperty
 import os
+
+# 201_損益分岐点設定項目画面に遷移
+class Gamen201Layout(BoxLayout):
+    pass
 
 class EventToolApp(App):
     def build(self):
@@ -11,11 +16,23 @@ class EventToolApp(App):
         LabelBase.register(name="NotoSansJP-Regular", fn_regular=font_path)
 
         Builder.load_file('main.kv')
+        Builder.load_file('gamen201.kv')
         return MainLayout()
 
 # メインレイアウトクラス
 class MainLayout(BoxLayout):
-    pass
+    header_text = StringProperty("作成したい表の項目を追加してください")
+    content = ObjectProperty(None)
+
+    # 名簿ボタン押下時の処理
+    def click_meibo(self):
+        self.header_text = "作成したい表の項目を追加してください"
+
+    # 収支ボタン押下時の処理
+    def click_syushi(self):
+        self.header_text = "収支　項目設定"
+        self.content.clear_widgets()
+        self.content.add_widget(Gamen201Layout())
 
 if __name__ == "__main__":
     EventToolApp().run()
